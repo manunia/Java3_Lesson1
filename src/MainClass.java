31c536e06dcd49530d9f0203275ad027dadb1cfd
manunia <timoshenkomaria@mail.ru>
2018-08-08 17:41:26 +0300

    test commit 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,11 @@
+/*
+created by Maria L
+ */
+//https://github.com/manunia/Java3_Lesson1.git
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Good buy");
     }
 
 }
